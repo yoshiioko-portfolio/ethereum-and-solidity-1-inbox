be162a1ab63d7007137f8b16d515a6782fbc80f6
yoshiioko <acardoza@pdx.edu>
2022-02-21 22:01:24 -0800

    implemented script to deploy contract to Rinkeby

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "smoke gravity lobster spot exhibit left satoshi leader auction renew room system",
+  "https://rinkeby.infura.io/v3/267f919664d449478d728b1adeead173"
+);
+const web3 = new Web3(provider);
+
+// Create async function to avoid using promise syntax
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
